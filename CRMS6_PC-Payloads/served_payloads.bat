ff3d998360d43bf84bf495928a4585a2d27b510d
std017011 <std017011@tesaban6.ac.th>
2025-08-12 09:32:55 +0700

    Update served_payloads.bat

--- CRMS6_PC-Payloads/served_payloads.bat
+++ CRMS6_PC-Payloads/served_payloads.bat
@@ -1,6 +1,13 @@
 @echo off
 title served_payloads.bat
 
+:: Get administrator permission
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
 :: Get the folder where this batch file is located
 set "basepath=%~dp0"
 
@@ -66,4 +73,5 @@ if exist "%software_payloads%" (
 
 pause
 goto menu
+
 
