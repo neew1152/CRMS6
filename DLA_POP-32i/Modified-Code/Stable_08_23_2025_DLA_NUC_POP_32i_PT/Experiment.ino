84fc504e0c64a535d6445e9be94994aacc25c231
std017011 <std017011@tesaban6.ac.th>
2025-08-23 16:49:27 +0700

    Add files via upload

---
+++ DLA_POP-32i/Modified-Code/Stable_08_23_2025_DLA_NUC_POP_32i_PT/Experiment.ino
@@ -0,0 +1,40 @@
+void Experiment() {
+  calibrate_IMU();
+  waitSW_A();
+  oled.clear();
+  oled.text(3, 0, "Running Laboratory...");
+  oled.show(); beep(1);
+  delay(500);
+
+  Lab_1();
+  Lab_2();
+  Lab_3();
+
+  oled.text(5, 4, "Successfully!");
+  oled.show(); beep(1);
+}
+
+// | ------------------------------------- |
+// | TracDegreeSpeedTime(80); Motorstop(); |
+// | ------------------------------------- |
+// | 2B |               300T               |
+// | 3B |               600T               |
+// | 4B |               900T               |
+
+// Forward_Balance(40, 250);
+// Backward_Balance(40, 400);
+
+void Lab_1() {
+  Backward_Balance(40, 400); TTL(90);
+}
+
+void Lab_2() {
+
+}
+
+void Lab_3() {
+
+}
+
+
+
