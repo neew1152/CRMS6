c8aadb0a11f2c38b1ae2a84b01d8749745e79e6c
std017011 <std017011@tesaban6.ac.th>
2025-08-28 08:08:15 +0700

    Add files via upload

---
+++ DLA_POP-32i/Modified-Code/Stable_08.24.2025-DLA_NUC_POP_32i_PT/Operation.ino
@@ -0,0 +1,45 @@
+void Operation() {
+  calibrate_IMU();
+  waitSW_OK();
+  oled.clear();
+  oled.text(3, 2, "Running Mission...");
+  oled.show();
+  beep(1);
+  delay(500);
+
+  Mission_1();
+  Mission_2();
+  Mission_3();
+  Mission_4();
+  Mission_5();
+
+  oled.text(5, 4, "Successfully!");
+  oled.show();
+  beep(1);
+}
+
+//  _______________________________________
+// | TracDegreeSpeedTime(80); Motorstop(); |
+// | ------------------------------------- |
+// | 2B |               300T               |
+// | 3B |               600T               |
+// | 4B |               900T               |
+
+// Forward_Balance(40, 250);
+// Backward_Balance(40, 400);
+
+void Mission_1() {
+  Forward_Balance(40, 250);
+}
+
+void Mission_2() {
+}
+
+void Mission_3() {
+}
+
+void Mission_4() {
+}
+
+void Mission_5() {
+}
