3b8aca49995fcfd4e65380807e6a11cfb8b2bc6e
neew1152 <128338887+neew1152@users.noreply.github.com>
2025-11-21 16:44:22 +0700

    Update payload.ps1

--- CRMS6_PC-toolkit/honeypot/payload.ps1
+++ CRMS6_PC-toolkit/honeypot/payload.ps1
@@ -1,8 +1,3 @@
 Write-Host "[ OK ] Disable all Spectre/Meltdown mitigations to mimic an unpatched system"
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverride" -Value 3 -Type DWord
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverrideMask" -Value 3 -Type DWord
-
-Write-Host "[ OK ] Change DNS"
-$adapter = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}
-
-Set-DnsClientServerAddress -InterfaceIndex $adapter.InterfaceIndex -ServerAddresses ("1.1.1.1", "8.8.8.8")
