93ffe098d84f8c164818b65a2ff84ab999a74075
std017011 <std017011@gmail.com>
2026-05-17 18:02:55 +0700

    Update PT_v06.6_Simplified.ino

--- DLA_PTBOT-Delivery/Source-Code/Simplified/PT_v06.6_Simplified/PT_v06.6_Simplified.ino
+++ DLA_PTBOT-Delivery/Source-Code/Simplified/PT_v06.6_Simplified/PT_v06.6_Simplified.ino
@@ -14,19 +14,19 @@ float previous_error_forward = 0;
 TaskHandle_t sonarTaskHandle = NULL;
 
 // IR                S0   S1   S2   S3
-int sensor_min[] = { 75, 95, 125, 120 };
-int sensor_max[] = { 580, 700, 780, 730 };
+int sensor_min[] = { 100, 100, 100, 100 };
+int sensor_max[] = { 500, 500, 500, 500 };
 
 // LASER
 float can_distance = 38;
 
 // SERVO
-int Swing_Left = 58;   // S0 เพิ่ม ง้างออก
-int Swing_Right = 58;  // S1 ลด หุบเข้า
-int Open_Left = 110;  // S2 เพิ่ม หุบเข้า
-int Open_Right = 90;  // S3 ลด หุบออก
-int Close_Left = 144;   // S2 เพิ่ม หุบเข้า
-int Close_Right = 124;  // S3 ลด หุบออก
+int Swing_Left = 45;
+int Swing_Right = 45;
+int Open_Left = 100;
+int Open_Right = 100;
+int Close_Left = 145;
+int Close_Right = 145;
 
 // ULTRASONIC
 float pick_can_distance = 7;
