fc41dc27e95b78a074a107a862b2123dcf755c44
std017011 <std017011@gmail.com>
2026-05-25 23:04:44 +0700

    Add files via upload

---
+++ DLA_PTBOT-Delivery/Source-Code/Simplified/PT_v07.3_NUC_Simplified/Arm.ino
@@ -0,0 +1,291 @@
+void SwingGripper(char gripper, int degree) {
+  switch (gripper) {
+    case 'l':
+      Swing_Current_Degree[0] = degree;
+      servoWrite(Channel_Swing_Left, Swing_Current_Degree[0]);
+      break;
+
+    case 'r':
+      Swing_Current_Degree[1] = degree;
+      servoWrite(Channel_Swing_Right, Swing_Current_Degree[1]);
+      break;
+  }
+}
+
+void LiftLoop(void *pvParameters) {
+  const int CLK = 39;
+  const int DT = 36;
+  float error, previous_error = 0, integral = 0, derivative;
+  float p_value, i_value, d_value, motor_speed;
+  float Kp = 3.0, Ki = 0.001, Kd = 0.05;
+
+  Encoder.attachHalfQuad(DT, CLK);
+  Encoder.setCount(0);
+
+  long last_Encoder_count = 0;
+  unsigned long last_change_time = millis();
+  const unsigned long timeout_ms = 500;
+
+  int last_Lift_Target = Lift_Target;
+  bool stopped_due_to_timeout = false;
+
+  while (1) {
+    long current_count = Encoder.getCount() / 2;
+    error = current_count - Lift_Target;
+
+    if (abs(current_count - last_Encoder_count) > 5) {
+      last_change_time = millis();
+      last_Encoder_count = current_count;
+      stopped_due_to_timeout = false;
+    }
+
+    if (Lift_Target != last_Lift_Target) {
+      stopped_due_to_timeout = false;
+      last_change_time = millis();
+      integral = 0;
+      previous_error = 0;
+      last_Lift_Target = Lift_Target;
+    }
+
+    if (!stopped_due_to_timeout && millis() - last_change_time > timeout_ms && abs(error) >= 5) {
+      if (xSemaphoreTakeRecursive(i2cMutex, portMAX_DELAY) == pdTRUE) {
+        motorWrite(3, 0);
+        xSemaphoreGiveRecursive(i2cMutex);
+      }
+      stopped_due_to_timeout = true;
+    }
+
+    if (stopped_due_to_timeout) {
+      continue;
+    }
+
+    p_value = Kp * error;
+    integral += error;
+    i_value = Ki * integral;
+    derivative = error - previous_error;
+    d_value = Kd * derivative;
+    motor_speed = p_value + i_value + d_value;
+
+    motor_speed = constrain(motor_speed, -100, 100);
+
+    if (abs(error) >= 5) {
+      if (xSemaphoreTakeRecursive(i2cMutex, portMAX_DELAY) == pdTRUE) {
+        motorWrite(3, motor_speed);
+        xSemaphoreGiveRecursive(i2cMutex);
+      }
+    } else {
+      if (xSemaphoreTakeRecursive(i2cMutex, portMAX_DELAY) == pdTRUE) {
+        motorWrite(3, 0);
+        xSemaphoreGiveRecursive(i2cMutex);
+      }
+    }
+
+    previous_error = error;
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+  }
+}
+
+void Lift(int position) {
+  if (position < 0) position = 0;
+  Lift_Target = position * 28;
+  if (Lift_Target > 900) Lift_Target = 900;
+}
+
+void FFobj(int obj_height) {
+  if (obj_height != 0) FFUltrasonic(Ultrasonic_Pick_Distance);
+  else FFLaser(Laser_Distance);
+  vTaskDelay(250 / portTICK_PERIOD_MS);
+}
+
+void FFobj(int obj_height, char gripper) {
+  switch (gripper) {
+    case 'L':
+      SwingGripper('r', Swing_Right + 60);
+      break;
+
+    case 'R':
+      SwingGripper('l', Swing_Left + 60);
+      break;
+  }
+  FFobj(obj_height);
+}
+
+void PickCan(int pick_height, int B_Speed, int B_delay) {
+  PickCan(pick_height, 'A', B_Speed, B_delay);
+}
+
+void PickCan(int pick_height, char gripper, int B_Speed, int B_delay) {
+  BalanceArm('O');
+  Lift(pick_height);
+  FFobj(pick_height, gripper);
+  switch (gripper) {
+    case 'L':
+      servoWrite(Channel_Gripper_Left, Close_Left);
+      break;
+
+    case 'R':
+      servoWrite(Channel_Gripper_Right, Close_Right);
+      break;
+
+    default:
+      servoWrite(Channel_Gripper_Left, Close_Left);
+      servoWrite(Channel_Gripper_Right, Close_Right);
+      break;
+  }
+  vTaskDelay(250 / portTICK_PERIOD_MS);
+  Lift(pick_height + 4);
+  vTaskDelay(250 / portTICK_PERIOD_MS);
+  BB(25, 500);
+  StableCAN();
+  BB(B_Speed, B_delay);
+}
+
+void DropCan(int Drop_height, char gripper, char Drop_point, int B_Speed, int B_delay) {
+  Lift(Drop_height + 2);
+  int delay_time = 250 + (Drop_height * 70);
+  if (delay_time < 0) delay_time = 0;
+  vTaskDelay(delay_time / portTICK_PERIOD_MS);
+  motorStop();
+  switch (gripper) {
+    case 'L':
+      switch (Drop_point) {
+        case 'L':
+          SwingGripper('l', Swing_Left + 10);
+          SwingGripper('r', Swing_Right + 60);
+          break;
+        case 'C':
+          SwingGripper('l', Swing_Left - 20);
+          SwingGripper('r', Swing_Right + 60);
+          break;
+        case 'A':
+          servoWrite(Channel_Swing_Left, Swing_Left);
+          servoWrite(Channel_Swing_Right, Swing_Right + 60);
+          break;
+      }
+      break;
+
+    case 'R':
+      switch (Drop_point) {
+        case 'R':
+          SwingGripper('l', Swing_Left + 60);
+          SwingGripper('r', Swing_Right + 10);
+          break;
+        case 'C':
+          SwingGripper('l', Swing_Left + 60);
+          SwingGripper('r', Swing_Right - 20);
+          break;
+        case 'A':
+          servoWrite(Channel_Swing_Left, Swing_Left + 60);
+          servoWrite(Channel_Swing_Right, Swing_Right);
+          break;
+      }
+      break;
+
+    case 'A':
+      switch (Drop_point) {
+        case 'A':
+          servoWrite(Channel_Swing_Left, Swing_Left);
+          servoWrite(Channel_Swing_Right, Swing_Right);
+          break;
+      }
+      break;
+  }
+
+  FFobj(Drop_height);
+  Lift(Drop_height);
+  vTaskDelay(250 / portTICK_PERIOD_MS);
+  DropPosition(gripper);
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  BB(25, 500);
+  BalanceArm('S');
+  Lift(0);
+  vTaskDelay(delay_time / portTICK_PERIOD_MS);
+  Lift(Lift_Default);
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+  BB(B_Speed, B_delay);
+}
+
+void DropPosition(char gripper) {
+  switch (gripper) {
+    case 'L':
+      servoWrite(Channel_Gripper_Left, Close_Left - 10);
+      vTaskDelay(250 / portTICK_PERIOD_MS);
+      servoWrite(Channel_Gripper_Left, Open_Left);
+      break;
+
+    case 'R':
+      servoWrite(Channel_Gripper_Right, Close_Right - 10);
+      vTaskDelay(250 / portTICK_PERIOD_MS);
+      servoWrite(Channel_Gripper_Right, Open_Right);
+      break;
+
+    case 'A':
+      servoWrite(Channel_Gripper_Left, Close_Left - 10);
+      servoWrite(Channel_Gripper_Right, Close_Right - 10);
+      vTaskDelay(250 / portTICK_PERIOD_MS);
+      servoWrite(Channel_Gripper_Left, Open_Left);
+      servoWrite(Channel_Gripper_Right, Open_Right);
+      break;
+  }
+}
+
+void BalanceArm(char position) {
+  switch (position) {
+    case 'C':
+      servoWrite(Channel_Gripper_Left, Close_Left);
+      servoWrite(Channel_Gripper_Right, Close_Right);
+      servoWrite(Channel_Swing_Left, Swing_Left - 30);
+      servoWrite(Channel_Swing_Right, Swing_Right - 30);
+      break;
+
+    case 'O':
+      servoWrite(Channel_Swing_Left, Swing_Left);
+      servoWrite(Channel_Swing_Right, Swing_Right);
+      servoWrite(Channel_Gripper_Left, Open_Left);
+      servoWrite(Channel_Gripper_Right, Open_Right);
+      break;
+
+    case 'L':
+      servoWrite(Channel_Gripper_Left, Close_Left);
+      servoWrite(Channel_Gripper_Right, Close_Right);
+      servoWrite(Channel_Swing_Left, Swing_Left + 100);
+      servoWrite(Channel_Swing_Right, Swing_Right - 30);
+      break;
+
+    case 'R':
+      servoWrite(Channel_Gripper_Left, Close_Left);
+      servoWrite(Channel_Gripper_Right, Close_Right);
+      servoWrite(Channel_Swing_Right, Swing_Right + 100);
+      servoWrite(Channel_Swing_Left, Swing_Left - 30);
+      break;
+
+    case 'S':
+      servoWrite(Channel_Swing_Left, Swing_Left);
+      servoWrite(Channel_Swing_Right, Swing_Right);
+      break;
+
+    case 'W':
+      servoWrite(Channel_Gripper_Left, Close_Left);
+      servoWrite(Channel_Gripper_Right, Close_Right);
+      servoWrite(Channel_Swing_Left, Swing_Left + 100);
+      servoWrite(Channel_Swing_Right, Swing_Right + 100);
+      break;
+  }
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+}
+
+void StableCAN() {
+  Lift(0);
+  BB(10, 300);
+  vTaskDelay(250 / portTICK_PERIOD_MS);
+  servoWrite(Channel_Gripper_Left, Close_Left - 15);
+  servoWrite(Channel_Gripper_Right, Close_Right - 15);
+  vTaskDelay(250 / portTICK_PERIOD_MS);
+  FF(10, 300);
+  vTaskDelay(250 / portTICK_PERIOD_MS);
+  servoWrite(Channel_Gripper_Left, Close_Left);
+  servoWrite(Channel_Gripper_Right, Close_Right);
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+  Lift(Lift_Default);
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+}
