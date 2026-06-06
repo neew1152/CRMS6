0855802c6717adb37a317a8a54e7cb3e858f2d15
std017011 <std017011@gmail.com>
2026-06-06 11:54:58 +0700

    Add files via upload

---
+++ DLA_PTBOT-Delivery/Source-Code/Simplified/PT_v08.0_CAP_Simplified/Operation.ino
@@ -0,0 +1,153 @@
+/////////////////////////////////////////////////
+
+// PickCan(5, 60, 275);
+// DropCan(5, 'A', 'A', 60, 275);
+// SetBack(25, 300); SetFront(25, 350);
+
+/////////////////////////////////////////////////
+
+//  Chunks  |
+//    1     | FF(100, 425);
+//    2     | FF(100, 630);
+//    3     | FF(100, 840);
+
+/////////////////////////////////////////////////
+
+//  Chunks  |
+//    1     | FF(60, 535);
+//    2     | FF(60, 855);
+//    3     | FF(60, 1175);
+// High Way | FF(60, 1500);
+
+/////////////////////////////////////////////////
+
+//   Chunks   |
+//     1      | FF(40, 700);
+//     2      | FF(40, 1210);
+//     3      | FF(40, 1720);
+//  High Way  | FF(40, 2200); | 1250 & 1175
+// Chopsticks | FF(20, 3100);
+
+/////////////////////////////////////////////////
+
+//        IN          |   OUT
+// SetFront(25, 300); | SetFront(25, 340);
+// SetBack(25, 350);  | SetBack(25, 410);
+
+/////////////////////////////////////////////////
+
+void Operation() {
+  Mission_1();
+  STOP();
+  Mission_2();
+  STOP();
+  Mission_3();
+  STOP();
+  Mission_4();
+}
+
+void Mission_1() {
+  BalanceArm('C');
+  BB(60, 1175);
+  SetBack(25, 350, 'L');
+  FF(60, 855);
+  SetFront(25, 300, 'L');
+  BB(60, 500, 'L');
+  FF(60, 525, 'R');
+  SetFront(25, 300);
+  BB(60, 525, 'L');
+  SetFront(25, 300);
+  BB(60, 1500, 'R');
+  BB(60, 535);
+  SetBack(25, 200, 'L');
+  BalanceArm('W');
+  FF(60, 855, 'R');
+  SetBack(25, 300, 'L');
+  PickCan(5, 60, 275);
+
+  BalanceArm('C');
+  BB(60, 855);
+  SetBack(25, 410, 'R');
+  FF(60, 525, 'L');
+  BB(60, 535, 'R');
+  BB(60, 535);
+  FF(60, 500);
+  SetFront(25, 340, 'L');
+  BB(60, 535);
+  SetBack(25, 300, 'L');
+  DropCan(10, 'L', 'L', 60, 275);
+
+  SpinDegree(90);
+  BalanceArm('C');
+  FF(60, 855, 'R');
+  FF(60, 535, 'R');
+  FF(60, 535, 'R');
+  BB(60, 535);
+  SetBack(25, 300, 'L');
+  DropCan(13, 'R', 'R', 60, 275);
+}
+
+void Mission_2() {
+  SpinDegree(90);
+  FF(60, 535);
+  SetFront(25, 350, 'L');
+  PickCan(0, 60, 300);
+
+  BalanceArm('W');
+  BB(60, 535);
+  SetBack(25, 410, 'R');
+  FF(60, 535, 'R');
+  BB(60, 855);
+  SetBack(25, 300, 'L');
+  DropCan(10, 'L', 'L', 60, 275);
+
+  SpinDegree(90);
+  BalanceArm('C');
+  SetBack(25, 350);
+  FF(60, 1500 + 535);
+  SetFront(25, 350, 'L');
+  DropCan(5, 'L', 'L', 60, 275);
+}
+
+void Mission_3() {
+  SpinDegree(90);
+  BalanceArm('C');
+  SetFront(25, 300);
+  BB(60, 1500, 'L');
+  BB(60, 885);
+  SetBack(25, 350, 'R');
+  FF(60, 855);
+  SetFront(25, 340, 'L');
+  PickCan(5, 60, 275);
+
+  SetBack(25, 350, 'L');
+  BalanceArm('C');
+  FF(60, 855);
+  SetFront(25, 300, 'R');
+  FF(60, 535, 'L');
+  FF(60, 535, 'R');
+  BB(60, 535);
+  SetBack(25, 300, 'L');
+  DropCan(13, 'L', 'L', 60, 275);
+
+  SpinDegree(90);
+  BalanceArm('C');
+  FF(60, 535);
+  SetFront(25, 350, 'L');
+  BB(60, 535);
+  SetBack(25, 410, 'L');
+  BB(60, 535, 'R');
+  BB(60, 1500);
+  SetBack(25, 300, 'R');
+  DropCan(5, 'R', 'R', 60, 275);
+}
+
+void Mission_4() {
+  BalanceArm('C');
+  BB(60, 535);
+  SetBack(25, 350, 'R');
+  BB(60, 525, 'R');
+  FF(60, 515, 'R');
+  BB(60, 855, 'R');
+  FF(60, 1100);
+}
