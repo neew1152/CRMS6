380ce05c06b5bc11dac300684c361833b4dc1141
neew1152 <neew2166@gmail.com>
2026-09-01 18:28:53 +0700

    Delete CRMS6_PC-toolkit directory

--- CRMS6_PC-toolkit/honeypot/payload.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "[ OK ] Disable all Spectre/Meltdown mitigations to mimic an unpatched system"
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverride" -Value 3 -Type DWord
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettingsOverrideMask" -Value 3 -Type DWord
